8f04282a962005556a71851ad2bdfcafa8ea404c
Maurício <mauricioreiscds@gmail.com>
2022-02-02 18:14:26 -0300

    Feat: finished login js

--- scripts/password-logical.js
+++ scripts/password-logical.js
@@ -1,3 +1,25 @@
 export default function passwordLogical() {
-    alert("Está funcionando")
+
+    const $ = document.querySelector.bind(document)
+    const $$ = document.querySelectorAll.bind(document)
+
+    const loginPage = $("#login-page")
+    const homePage = $("#home-page")
+
+    const user = $("#input-text")
+    const password = $("#input-password")
+
+    const errorMensage = $("#message-error p")
+    const button = $("#button-continue")
+
+    button.addEventListener("click", () => {
+        if (user.value !== "admin" || password.value !== "admin"){
+            errorMensage.style.display = "block"
+            user.style.border =  "1px solid #E9B425"
+            password.style.border =  "1px solid #E9B425"
+        } else {
+            loginPage.style.display = "none";
+            homePage.style.display = "block";
+        }
+    })
 }
