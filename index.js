7484dda75097dfa2491b47e076cfd8c6a2172782
Maurício <mauricioreiscds@gmail.com>
2022-02-04 18:39:07 -0300

    Doc: js file created

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import passwordLogical from "./scripts/password-logical.js"
+import clockLogical from "./scripts/clock-logical"
+
 passwordLogical()
+
+setInterval(() => {
+    clockLogical()
+}, 1000);
 
 
---
+++ scripts/clock-logical.js
@@ -0,0 +1,3 @@
+export default function clockLogical(){
+    alert("TA OK")
+}
