3015447b1f2a7239367c2668be0a27eb1a01047f
Maurício <mauricioreiscds@gmail.com>
2022-02-06 01:27:52 -0300

    Feat: weather js completed

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ import passwordLogical from "./scripts/password-logical.js"
 import clockLogical from "./scripts/clock-logical.js"
 import logout from "./scripts/logout-logical.js"
 import weatherLogical from "./scripts/weather-logical.js"
-import iconWeather from "./scripts/icon-weather.js"
 
 passwordLogical()
 logout()
 weatherLogical()
-iconWeather()
 
 setInterval(() => {
     clockLogical()
